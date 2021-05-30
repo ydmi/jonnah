0e04c9cf13dbf22751bac53798a606c314a06b1b
ydmi <82627330+ydmi@users.noreply.github.com>
2021-05-30 22:48:41 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+$servername = "remotemysql.com";
+$username = "hO92SgBK9V";
+$password = "JOI1AX3gmo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
